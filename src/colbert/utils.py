f0f9a3c46029c42a21218062e4a4d01dd4a39ff0
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-11-06 17:40:14 +0100

    Ajout d'une sous-fonction rst_table_row() à utils.rst_table().

--- src/colbert/utils.py
+++ src/colbert/utils.py
@@ -42,46 +42,51 @@ def rst_section(title, symbol="="):
 def rst_table(table):
     header_stroke_char = '='
     normal_stroke_char = '-'
-
     lines = []
+    last_iteration = len(table) - 1
     for i, row in enumerate(table):
-        stroke, line = [], []
         stroke_char = (i == 1) and header_stroke_char or normal_stroke_char
-        # Multiline row.
-        if isinstance(row[0][0], list) or isinstance(row[0][0], tuple):
-            for j, subrow in enumerate(row):
-                line = []
-                for cell_content, cell_length in subrow:
-                    cell_content = truncate_words(cell_content, cell_length - 2)
-                    # Idéalement, on devrait faire le stroke sur la dernière sous-ligne, pas la premiere.
-                    if j == 0:
-                        stroke.append(u'+%s' % (stroke_char * cell_length))
-                    line.append(u'|| %s%s' % (cell_content,
-                                              u' ' * (cell_length - 2 - len(cell_content))))
-                if j == 0:
-                    stroke.append(u'+')
-                    lines.append(u''.join(stroke))
+        add_closing_stroke = (i == last_iteration) and True
+        lines.extend(rst_table_row(row, stroke_char, add_closing_stroke))
+    lines = u"\n".join(lines)
+    return lines
 
-                line.append(u'|')
-                lines.append(u''.join(line))
 
-        else:
-            for cell_content, cell_length in row:
+def rst_table_row(row, stroke_char, add_closing_stroke=False):
+    lines = []
+    stroke, line = [], []
+    if isinstance(row[0][0], list) or isinstance(row[0][0], tuple):
+        for j, subrow in enumerate(row):
+            line = []
+            for cell_content, cell_length in subrow:
                 cell_content = truncate_words(cell_content, cell_length - 2)
-                stroke.append(u'+%s' % (stroke_char * cell_length))
-                line.append(u'| %s%s' % (cell_content,
-                                         u' ' * (cell_length - 1 - len(cell_content))))
-            # End of line.
-            stroke.append(u'+')
+                # Idéalement, on devrait faire le stroke sur la dernière sous-ligne, pas la premiere.
+                if j == 0:
+                    stroke.append(u'+%s' % (stroke_char * cell_length))
+                line.append(u'|| %s%s' % (cell_content,
+                                            u' ' * (cell_length - 2 - len(cell_content))))
+            if j == 0:
+                stroke.append(u'+')
+                lines.append(u''.join(stroke))
+
             line.append(u'|')
+            lines.append(u''.join(line))
+    else:
+        for cell_content, cell_length in row:
+            cell_content = truncate_words(cell_content, cell_length - 2)
+            stroke.append(u'+%s' % (stroke_char * cell_length))
+            line.append(u'| %s%s' % (cell_content,
+                                        u' ' * (cell_length - 1 - len(cell_content))))
+        # End of line.
+        stroke.append(u'+')
+        line.append(u'|')
 
-            lines.append(''.join(stroke))
-            lines.append(''.join(line))
+        lines.append(''.join(stroke))
+        lines.append(''.join(line))
+    if add_closing_stroke:
+        lines.append(''.join(stroke))
 
-    # A final line of strokes.
-    lines.append(''.join(stroke))
-    return u"\n".join(lines)
-
+    return lines
 
 # http://stackoverflow.com/questions/250357/smart-truncate-in-python
 def truncate_words(content, length=100, suffix='...'):
