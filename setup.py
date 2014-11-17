481bbf138e11cf66343523064b1a970beb5f7c2c
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-11-17 10:01:42 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name="Colbert",
-    version="0.15",
+    version="0.16",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
